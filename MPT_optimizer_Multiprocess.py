7f57ff1bf610c1f8150ca5fce7bba44b23b1e2d2
DenUil.laptop <rej.brouns@gmail.com>
2019-05-30 11:35:14 +0200

    Added comparison to optimiser, other files are from pycharm IDE

--- MPT_optimizer_Multiprocess.py
+++ MPT_optimizer_Multiprocess.py
@@ -82,6 +82,8 @@ def minimizerThread(q, threadIndex):
 
 if __name__ == "__main__":
     globalStopWatchStart = datetime.now()
+    prevOptPortfolio=None
+
     #Definitions
     #select the tickers of the equities you are interested in
     symbolsBE = ["ABI.BR", "ABO.BR", "ACCB.BR", "ACKB.BR", "AED.BR", "AGS.BR", "AGFB.BR", "ANT.BR", "ARGX.BR", "ASC.BR",
@@ -222,15 +224,49 @@ if __name__ == "__main__":
         optPortTickers, optPortFinalWeights, optPortReturns, optPortVolatility, optPortSharpe = optimalPortfolio
         endStopWatch = time.time()
 
+        #compare how previous optimal portfolio does with current dataset
+        if prevOptPortfolio == [] :
+            prevOptPortfolio = (optPortTickers, optPortFinalWeights)
+            prevOptPortTickers , prevOptPortFinalWeights = None, None
+        else :
+            #recalculate previous portfolio with current trainingset
+            prevOptPortTickers , prevOptPortFinalWeights = prevOptPortfolio
+            cIndex = []
+            for co in prevOptPortTickers:
+                cIndex.append(table.columns.get_loc(co))
+            #Create a subset of the training data that just has the equities in the combination
+            newTable = table.iloc[:, cIndex]
+            num_assets = len(prevOptPortTickers)
+            # calculate daily and annual returns of the stocks
+            returns_daily = newTable.pct_change()
+            returns_annual = returns_daily.mean() * 250
+
+            # get daily and covariance of returns of the stock
+            cov_daily = returns_daily.cov()
+            cov_annual = cov_daily * 250
+
+            #calculate returns volatility sharpe ratio
+            prevReturns = np.dot(prevOptPortFinalWeights, returns_annual)
+            prevVolatility = np.sqrt(np.dot(prevOptPortFinalWeights.T, np.dot(cov_annual, prevOptPortFinalWeights)))
+            prevSharpe = (prevReturns - 0.03) / prevVolatility
+
+            # store current portfolio for comparison next run
+            prevOptPortfolio = (optPortTickers, optPortFinalWeights)
+
+
         #report data
         print("----------------------------------------------------------------")
         print("----------------------------------------------------------------")
         print("Training data interval: {} - {}".format(startTrainingData, endTrainingData))
-
         print("Time to completion:{}".format(endStopWatch-startStopWatch))
-
         print("Max Sharpe Ratio: {} for Portofolio {} with respectively weights {}".format(optPortSharpe,optPortTickers,optPortFinalWeights))
         print("Volatility: {} and return: {}".format(optPortVolatility,optPortReturns))
+        if prevOptPortTickers != None :
+            print("Previous portfolio gave us:")
+            print("Max Sharpe Ratio: {} for Portofolio {} with respectively weights {}".format(prevSharpe, prevOptPortTickers,prevOptPortFinalWeights))
+            print("Volatility: {} and return: {}".format(prevVolatility, prevReturns))
+
+
         print("----------------------------------------------------------------")
         print("----------------------------------------------------------------")
 
